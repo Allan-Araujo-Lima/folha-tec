15cdc6544667d6e3ee49886e0c782ccadd54d364
Allan Araujo Lima <62760029+Allan-Araujo-Lima@users.noreply.github.com>
2024-07-18 23:12:43 -0300

    style: adição de carrossel sobre a missão, visão e valores da empresa

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,35 +1,69 @@
-import React from 'react';
-import './styles.css'
+import React, { useRef, useState, useEffect } from 'react';
+import './styles.css';
 
 export const Home = () => {
+    const [currentItem, setCurrentItem] = useState(1);
+    const itemsRef = useRef([]);
+
+    useEffect(() => {
+        const items = document.querySelectorAll('.item');
+        itemsRef.current = items;
+        check();
+    }, []);
+
+    const maxItems = itemsRef.current.length;
+
+    function check() {
+        itemsRef.current.forEach((item) => item.classList.remove('active'));
+
+        if (itemsRef.current[currentItem]) {
+            itemsRef.current[currentItem].scrollIntoView({
+                inline: "center",
+                behavior: "smooth"
+            });
+            itemsRef.current[currentItem].classList.add('active');
+        }
+    }
+
+    function left() {
+        setCurrentItem((prevItem) => {
+            const newItem = prevItem > 0 ? prevItem - 1 : maxItems - 1;
+            return newItem;
+        });
+    }
+
+    function right() {
+        setCurrentItem((prevItem) => {
+            const newItem = prevItem < maxItems - 1 ? prevItem + 1 : 0;
+            return newItem;
+        });
+    }
+
+    useEffect(() => {
+        check();
+    }, [currentItem]);
 
     return (
         <div className='introducao'>
             <h1 className='h1'>Bem-vindo(a) à <b>Folhatec!</b></h1>
-            <></>
             <p className='p'>Olá, somos a <b>Folhatec</b>, uma nova empresa que está preparada para revolucionar o mercado
                 de <b>departamento pessoal</b> do <b>Brasil!</b></p>
             <div className='carrossel' data-ride='carrossel'>
-                <ol
-                    className='carrossel-indicators'>
-                    <li data-target='carrossel' data-slide-to='0' className='active'></li>
-                    <li data-target='carrossel' data-slide-to='1'></li>
-                    <li data-target='carrossel' data-slide-to='2'></li>
-                </ol>
-                <div className='carrossel-inner'>
-                    <div className='carrossel-item active'>
-                        <div>
-                            <h1>teste 1</h1>
+                <button className='arrow-left control' aria-label='Previous' onClick={left}>◀︎</button>
+                <button className='arrow-right control' aria-label='Next' onClick={right}>▶︎</button>
+                <div className='carrossel-wrapper'>
+                    <div className='carrossel-inner'>
+                        <div className='carrossel-item item'>
+                            <h1 className='carrossel-h1'>Missão</h1>
+                            <p className='carrossel-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sint veniam explicabo, eveniet doloribus fugiat eos distinctio numquam obcaecati doloremque minima mollitia assumenda odio enim ut vel dolor dolorum fuga?</p>
                         </div>
-                    </div>
-                    <div className='carrossel-item'>
-                        <div>
-                            <h1>teste 2</h1>
+                        <div className='carrossel-item item active'>
+                            <h1 className='carrossel-h1'>Visão</h1>
+                            <p className='carrossel-p'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ex nemo cum fugit doloremque nisi hic autem veritatis delectus, natus sunt eligendi voluptas sint, dolorum doloribus veniam dolores earum omnis.</p>
                         </div>
-                    </div>
-                    <div className='carrossel-item'>
-                        <div>
-                            <h1>teste 3</h1>
+                        <div className='carrossel-item item'>
+                            <h1 className='carrossel-h1'>Valores</h1>
+                            <p className='carrossel-p'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat est sapiente ipsam. Atque repellendus itaque mollitia corporis consectetur. Et amet molestias officia similique necessitatibus natus repellat tempora. Nulla, nesciunt libero.</p>
                         </div>
                     </div>
                 </div>
