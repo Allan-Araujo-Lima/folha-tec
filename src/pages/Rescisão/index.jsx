29d8df429aedc90b47e9ef1104c663769c8decfe
Allan Araujo Lima <62760029+Allan-Araujo-Lima@users.noreply.github.com>
2024-10-03 23:45:58 -0300

    feat: separação de steps por tipo e implementação do step de remuneração.

--- src/pages/Rescisão/index.jsx
+++ src/pages/Rescisão/index.jsx
@@ -12,24 +12,12 @@ const experiencia = ["rescisaoAntecipaContratoExperienciaEmpregador", "rescisaoA
 const semAviso = ["porJustaCausa", "rescisaoContratoExperiencia", "morteEmpregado", experiencia[0], experiencia[1]];
 
 export const Rescisao = () => {
-    const [amount, setAmount] = useState("");
     const [form] = Form.useForm();
     const [current, setCurrent] = useState(0);
 
     const dataFormat = "DD/MM/YYYY"
 
     const diferenca = dayjs(form.getFieldValue("dataDemissao"), dataFormat).diff(form.getFieldValue("dataAdmissao"), "days")
-
-    const diasAvisoPrevio = dayjs(form.getFieldValue("dataDemissao"), dataFormat).diff(form.getFieldValue("dataAdmissao"), "year") * 3 + 30
-
-    const dataAvisoPrevio = dayjs(form.getFieldValue("dataDemissao"), dataFormat).add(diasAvisoPrevio, "day")
-
-    const handlerFocus = (e) => {
-        if (e.key === "Enter") {
-            e.preventDefaut()
-            next()
-        }
-    }
 
     const diasAvisoPrevio = dayjs(form.getFieldValue("dataDemissao"), dataFormat).diff(form.getFieldValue("dataAdmissao"), "year") * 3 + 30
 
@@ -101,113 +89,111 @@ export const Rescisao = () => {
             title: titleAviso,
             description:
                 <Form.Item required name={"tipoDeAviso"}>
-                    <Form.Item required name={"tipoDeAviso"}>
-                        {
-                            semAviso.includes(form.getFieldValue("tipoDeRescisao")) == false ?
-                                <Select
-                                    className="select tipoDeAviso"
-                                    onFocus={titleAviso = "Tipo de Aviso"}
-                                    disabled={current !== 3}
-                                    onChange={() => next()}>
-                                    <Select.Option value="avisoTrabalhado">Aviso prévio trabalhado</Select.Option>
-                                    <Select.Option value="avisoIndenizado">Aviso prévio indenizado</Select.Option>
-                                </Select>
-                                :
-                                experiencia.includes(form.getFieldValue("tipoDeRescisao")) == true ?
-                                    experiencia.includes(form.getFieldValue("tipoDeRescisao")) == true ?
-                                        <DatePicker
-                                            name="fimPrazoDeterminado"
-                                            onFocus={titleAviso = "Prazo final do contrato de experiência"}
-                                            format={dataFormat}
-                                            minDate={dayjs(form.getFieldValue("dataAdmissao"), dataFormat).add(1, 'day')}
-                                            maxDate={dayjs(form.getFieldValue("dataAdmissao"), dataFormat).add(89, 'day')}
-                                            disabled={current !== 3}
-                                            onChange={() => next()} />
-                                        :
-                                        <p> Tipo de rescisão sem aviso prévio.</p>
-                    }
-                    </Form.Item >,
-        },
                     {
-                        title: "Comunicado da rescisão",
-                    description:
-                    <div style={{ display: "flex", justifyContent: "space-between", textAlign: "center", lineHeight: "16px" }}>
-                        <Form.Item required name={"dataDemissao"}>
-                            <DatePicker
-                                format={dataFormat}
-                                minDate={dayjs(form.getFieldValue("dataAdmissao"), dataFormat)}
-                                disabled={current !== 4}
-                            />
-                        </Form.Item>
-                        {
-                            form.getFieldValue("dataDemissao") !== undefined && !semAviso.includes(form.getFieldValue("tipoDeRescisao")) ?
-                                `${diasAvisoPrevio} dias`
+                        semAviso.includes(form.getFieldValue("tipoDeRescisao")) == false ?
+                            <Select
+                                className="select tipoDeAviso"
+                                onFocus={titleAviso = "Tipo de Aviso"}
+                                disabled={current !== 3}
+                                onChange={() => next()}>
+                                <Select.Option value="avisoTrabalhado">Aviso prévio trabalhado</Select.Option>
+                                <Select.Option value="avisoIndenizado">Aviso prévio indenizado</Select.Option>
+                            </Select>
+                            :
+                            experiencia.includes(form.getFieldValue("tipoDeRescisao")) == true ?
+                                <DatePicker
+                                    name="fimPrazoDeterminado"
+                                    onFocus={titleAviso = "Prazo final do contrato de experiência"}
+                                    format={dataFormat}
+                                    minDate={dayjs(form.getFieldValue("dataAdmissao"), dataFormat).add(1, 'day')}
+                                    maxDate={dayjs(form.getFieldValue("dataAdmissao"), dataFormat).add(89, 'day')}
+                                    disabled={current !== 3}
+                                    onChange={() => next()} />
                                 :
-                                null
-                        }
-                        <Form.Item
-                            required
-                            disabled>
-                            {
-                                form.getFieldsValue("tipoDeAviso") !== "avisoTrabalhado" ?
-                                    <DatePicker name={"dataRescisao"} format={dataFormat} disabled placeholder="" value={form.getFieldValue("dataDemissao")} />
-                                    :
-                                    <DatePicker name={"dataRescisao"} format={dataFormat} disabled placeholder="" value={dayjs(form.getFieldValue("dataDemissao"), dataFormat).add(diasAvisoPrevio, "day")} />
-                            }
-                        </Form.Item>
-                    </div>,
+                                <p> Tipo de rescisão sem aviso prévio.</p>
+                    }
+                </Form.Item >,
         },
-                    ]
+        {
+            title: "Comunicado da rescisão",
+            description:
+                <div style={{ display: "flex", justifyContent: "space-between", textAlign: "center", lineHeight: "16px" }}>
+                    <Form.Item required name={"dataDemissao"}>
+                        <DatePicker
+                            format={dataFormat}
+                            minDate={dayjs(form.getFieldValue("dataAdmissao"), dataFormat)}
+                            disabled={current !== 4}
+                        />
+                    </Form.Item>
+                    {
+                        form.getFieldValue("dataDemissao") !== undefined && !semAviso.includes(form.getFieldValue("tipoDeRescisao")) ?
+                            `${diasAvisoPrevio} dias`
+                            :
+                            null
+                    }
+                    <Form.Item
+                        required
+                        disabled>
+                        {
+                            form.getFieldsValue("tipoDeAviso") !== "avisoTrabalhado" ?
+                                <DatePicker name={"dataRescisao"} format={dataFormat} disabled placeholder="" value={form.getFieldValue("dataDemissao")} />
+                                :
+                                <DatePicker name={"dataRescisao"} format={dataFormat} disabled placeholder="" value={dayjs(form.getFieldValue("dataDemissao"), dataFormat).add(diasAvisoPrevio, "day")} />
+                        }
+                    </Form.Item>
+                </div>,
+        },
+    ]
 
     const getUpdatedSteps = () => {
         return steps.map((step, index) => {
             if (index === current) {
-                return {...step, disabled: false, status: "process" };
+                return { ...step, disabled: false, status: "process" };
             } else {
-                return {...step, disabled: true, status: "wait" };
+                return { ...step, disabled: true, status: "wait" };
             }
         });
     };
 
-                    return (
-                    <Card>
-                        <Form layout="vertical" form={form}>
-                            <Card title="Informações rescisão">
-                                <Steps
-                                    className="steps"
-                                    direction="vertical"
-                                    current={current}
-                                    items={getUpdatedSteps()}>
-                                </Steps>
-                                {
-                                    form.getFieldValue("tipoDeRescisao") == "semJustaCausa" && form.getFieldValue("tipoDeAviso") == "avisoTrabalhado" && diasAvisoPrevio > 30 ?
-                                        <Checkbox>
-                                            Aviso trabalho de 30 dias e indenizar restante.
-                                        </Checkbox>
-                                        :
-                                        null
-                                }
-                                <div style={{ marginTop: 24 }}>
-                                    {current < steps.length - 1 && (
-                                        <Button type="primary" onClick={() => next()}>
-                                            Próximo
-                                        </Button>
-                                    )}
-                                    {current === steps.length - 1 && (
-                                        <Button type="primary" onClick={() => setCurrent(0)}>
-                                            Remuneração
-                                        </Button>
-                                    )}
-                                    {current > 0 && (
-                                        <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
-                                            Anterior
-                                        </Button>
-                                    )}
-                                </div>
-                            </Card>
-                            <Divider />
-                            <StepsRem />
-                        </Form>
-                    </Card>
-                    )
+    return (
+        <Card>
+            <Form layout="vertical" form={form}>
+                <Card title="Informações rescisão">
+                    <Steps
+                        className="steps"
+                        direction="vertical"
+                        current={current}
+                        items={getUpdatedSteps()}>
+                    </Steps>
+                    {
+                        form.getFieldValue("tipoDeRescisao") == "semJustaCausa" && form.getFieldValue("tipoDeAviso") == "avisoTrabalhado" && diasAvisoPrevio > 30 ?
+                            <Checkbox>
+                                Aviso trabalho de 30 dias e indenizar restante.
+                            </Checkbox>
+                            :
+                            null
+                    }
+                    <div style={{ marginTop: 24 }}>
+                        {current < steps.length - 1 && (
+                            <Button type="primary" onClick={() => next()}>
+                                Próximo
+                            </Button>
+                        )}
+                        {current === steps.length - 1 && (
+                            <Button type="primary" onClick={() => setCurrent(0)}>
+                                Remuneração
+                            </Button>
+                        )}
+                        {current > 0 && (
+                            <Button style={{ margin: '0 8px' }} onClick={() => prev()}>
+                                Anterior
+                            </Button>
+                        )}
+                    </div>
+                </Card>
+                <Divider />
+                <StepsRem />
+            </Form>
+        </Card>
+    )
 }
