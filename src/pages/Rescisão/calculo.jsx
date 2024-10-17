7f16ee2e96b2a4498c990e470a062b70d17156b4
Allan Araujo Lima <62760029+Allan-Araujo-Lima@users.noreply.github.com>
2024-10-16 23:20:15 -0300

    feat: implementação da lógica de cálculo das férias proporcionais e do 13° salário

--- src/pages/Rescisão/calculo.jsx
+++ src/pages/Rescisão/calculo.jsx
@@ -10,61 +10,86 @@ export const Calculo = ({ info }) => {
 
     const dataFormat = "DD/MM/YYYY";
 
-    let diasAvisoPrevio = info?.dataDemissao && info?.dataAdmissao ? dayjs(info.dataDemissao, dataFormat).diff(info.dataAdmissao, "year") * 3 + 30 : 30;
-    if (diasAvisoPrevio > 90) diasAvisoPrevio = 90;
-
-    const progecaoAviso = dayjs(info.dataDemissao).add(diasAvisoPrevio, "day").format(dataFormat)
-
     const submit = () => {
         //informações base
         const salarioBase = info.salarioBase;
         const insalubridade = info.adicionais == "insalubridade" ? SalarioMinimo * info.insalubridade / 100 : 0;
         const periculosidade = info.adicionais == "insalubridade" ? salarioBase * info.insalubridade / 100 : 0;
-        const salarioBaseAdicionais = salarioBase + insalubridade + periculosidade
+        const salarioBaseAdicionais = salarioBase + insalubridade + periculosidade;
+
+        //datas e quantidade de dias
+        let diasAvisoPrevio = info?.dataDemissao && info?.dataAdmissao ? dayjs(info.dataDemissao, dataFormat).diff(info.dataAdmissao, "year") * 3 + 30 : 30;
+        if (diasAvisoPrevio > 90) diasAvisoPrevio = 90;
+        const dataDemissao = info.tipoDeAviso === "avisoTrabalhado" ? dayjs(info.dataDemissao).add(diasAvisoPrevio, "day") : info.dataDemissao;
 
         //definição da base de cálculo de cada rúbrica específica
         const baseAviso = salarioBaseAdicionais + info?.variavelRescisao;
-        const baseFeriasVencidas = info.variavelFeriasVencidas > 0 ? salarioBaseAdicionais + info?.variavelFeriasVencidas : salarioBaseAdicionais;
-        const baseFeriasProporcionais = salarioBaseAdicionais + info?.variavelFeriasProporcionais;
-        const baseDecimo = salarioBaseAdicionais + info?.variavelDecimo;
+        const baseFeriasVencidas = info.variavelFeriasVencidas > 0 ? salarioBaseAdicionais + info.variavelFeriasVencidas : salarioBaseAdicionais;
+        const baseFeriasProporcionais = info.variavelFeriasProporcionais > 0 ? salarioBaseAdicionais + info.variavelFeriasProporcionais : salarioBaseAdicionais;
+        const baseDecimo = info.variavelDecimo > 0 ? salarioBaseAdicionais + info.variavelDecimo : salarioBaseAdicionais;
 
         //verifica o tipo de rescisão
+        let valorAviso = 0;
+
         if (rescisoesAviso.includes(info.tipoDeRescisao)) {
             //falta lógica
         } else if (indExperiencia.includes(info.tipoDeRescisao)) {
-            const diasIndenizacaoExperiencia = dayjs(info.fimPrazoDeterminado).diff(info.dataDemissao)
+            const diasIndenizacaoExperiencia = dayjs(info.fimPrazoDeterminado).diff(dataDemissao)
             const indenizacaoExperiencia = baseAviso / 30 * diasIndenizacaoExperiencia / 2
             if (info.tipoDeRescisao == "rescisaoAntecipaContratoExperienciaEmpregador") {
                 //falta lógica
             } else {
                 //falta lógica
             }
-        } else {
-            //falta lógica
         }
 
         //férias vencidas
-        const diferencaAnoAdmissaoDemissao = dayjs(info.dataDemissao).year() - dayjs(info.dataAdmissao).year() - 1;
+        const diferencaAnoAdmissaoDemissao = dayjs(dataDemissao).year() - dayjs(info.dataAdmissao).year() - 1;
         const inicioPeriodoAquisitivoFeriasVencidas = dayjs(info.dataAdmissao).add(diferencaAnoAdmissaoDemissao, "year").format(dataFormat);
-        if (dayjs(inicioPeriodoAquisitivoFeriasVencidas).isBefore(info.dataDemissao, "day") === true) {
+        if (dayjs(inicioPeriodoAquisitivoFeriasVencidas).isBefore(dataDemissao, "day") === true) {
             const feriasVencidas = baseFeriasVencidas / 30 * (30 - info?.feriasVencidas);
             const tercoFeriasVencidas = feriasVencidas / 3;
         }
+
         //férias proporcionais
         const inicioPeriodoAquisitivoFeriasProporcionais = dayjs(inicioPeriodoAquisitivoFeriasVencidas, dataFormat).add(1, "year");
-        console.log(dayjs(inicioPeriodoAquisitivoFeriasProporcionais).format(dataFormat))
-        let periodoAquisitivoFeriasProporcionaisMes = dayjs(info.dataDemissao).diff(inicioPeriodoAquisitivoFeriasProporcionais, "months");
-        console.log(periodoAquisitivoFeriasProporcionaisMes)
-        if (dayjs(inicioPeriodoAquisitivoFeriasProporcionais).add(periodoAquisitivoFeriasProporcionaisMes, "month").diff(info.dataDemissao, "day") * -1 + 1 >= 15) {
+        let periodoAquisitivoFeriasProporcionaisMes = dayjs(dataDemissao).diff(inicioPeriodoAquisitivoFeriasProporcionais, "months");
+        if (dayjs(inicioPeriodoAquisitivoFeriasProporcionais).add(periodoAquisitivoFeriasProporcionaisMes, "month").diff(dataDemissao, "day") * -1 + 1 >= 15) {
             periodoAquisitivoFeriasProporcionaisMes++
-        }
+        };
+
+        const feriasProporcionais = info.feriasColetivas ? baseFeriasProporcionais / 30 * periodoAquisitivoFeriasProporcionaisMes * 2.5 - info.feriasColetivas : baseFeriasProporcionais / 30 * periodoAquisitivoFeriasProporcionaisMes * 2.5;
+        const tercoFeriasProporcionais = feriasProporcionais / 3
 
         //13° salário
+        let avosDecimo = -1;
+
+        let inicioDecimo = dayjs(dataDemissao).startOf("year");
+        if (dayjs(info.dataAdmissao).isAfter(dayjs(inicioDecimo))) {
+            inicioDecimo = dayjs(info.dataAdmissao);
+            // Verifica se trabalhou mais de 15 dias no mês da admissão
+            if (dayjs(dayjs(inicioDecimo).endOf("month")).diff(inicioDecimo, "day") + 1 >= 15) {
+                avosDecimo++;
+            };
+        };
+
+        // Calcula os avos de décimo terceiro
+        avosDecimo += dayjs(dataDemissao).month() - dayjs(inicioDecimo).month();
+
+        // Identifica o começo do mês de rescisão
+        const comecoMesRescisao = dayjs(dataDemissao).startOf("month");
+
+        // Verifica se há mais de 15 dias no último mês de trabalho
+        if (dayjs(dataDemissao).diff(comecoMesRescisao, "day") + 1 >= 15) {
+            avosDecimo++;
+        };
+
+        const valorDecimo = baseDecimo / 12 * avosDecimo;
     }
 
     return (
         <div>
-            <Button onClick={console.log(submit())}></Button>
+            <Button onClick={submit()}></Button>
         </div>
     )
 }
